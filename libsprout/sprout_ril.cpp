0ad83a89b8d4821211ae508c2e346daf3d150e0a
ujwalp15 <ujwalp09@gmail.com>
2016-12-24 19:10:37 +0600

    sprout-common: sprout_ril: Add missing RIL symbol

--- libsprout/sprout_ril.cpp
+++ libsprout/sprout_ril.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,4 +20,8 @@ extern "C" void RIL_register_socket() {}
 extern "C" int _ZN7android6Parcel13writeString16EPKDsj();
 extern "C" int _ZN7android6Parcel13writeString16EPKtj() {
     return _ZN7android6Parcel13writeString16EPKDsj();
+}
+extern "C" int _ZN7android6Parcel13writeString16EPKtjparcel_writeString();
+extern "C" int _ZN7android6Parcel13writeString16EPKtjparcel_writeString() {
+    return _ZN7android6Parcel13writeString16EPKtjparcel_writeString();
 }
